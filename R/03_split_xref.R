9ce3b38c151faf76d783193b6e29a4c6019d2b4d
Sowmya Parthiban <sparthi1@jh.edu>
2022-12-27 23:21:13 +0530

    create column for each id: if ID in xref 1, else NA

--- R/03_split_xref.R
+++ R/03_split_xref.R
@@ -10,6 +10,38 @@ library("readr")
 library("dplyr")
 
 #### LOAD ORPHA DATA #####
-orpha <- read_csv(here("data", "orpha.csv"))
-human_do <- read_csv(here("data", "human_do.csv"))
+orpha <- read_csv(here("data", "ordo_post_02.csv"))
+human_do <- read_csv(here("data", "human_do_post_02.csv"))
+
+#### 1/0 columns for presence of ID in xref ####
+
+#Below are all possible DBs ORPHA is linked to.
+# > unique(db_list_orpha)
+# [1] "icd10"     "mesh"      "meddra"    "umls"      "omim"
+# [6] "icd11"     "ensembl"   "genatlas"  "hgnc"      "swissprot"
+# [11] "reactome"  "iuphar"
+
+
+db_list_ordo <- c("icd10", "mesh", "meddra", "umls", "omim","icd11" ,"ensembl",
+                  "genatlas", "hgnc", "swissprot", "reactome" , "iuphar")
+
+
+for (id in db_list_ordo) {
+    eval(parse(text = paste0('orpha$', id, 'present', ' <- ifelse(grepl(id , orpha$xref), yes = 1 , no = NA)')))
+
+}
+
+db_list_human_do <- c("icdo", "mesh" ,"nci" , "snomedct_us_2022_03_01" , "umls_cui", "icd10cm" ,
+                      "icd9cm","snomedct_us_2021_09_01", "ordo", "gard", "omim"  ,
+                      "efo"  , "kegg",  "meddra", "snomedct_us_2021_07_31",
+                      "snomedct_us_2022_10_31", "icd11"  , "snomedct_us_2022_07_31",  "snomedct_us_2020_09_01" ,
+                      "snomedct_us_2020_03_01")
+
+for (id in db_list_human_do) {
+    eval(parse(text = paste0('human_do$', id, 'present', ' <- ifelse(grepl(id , human_do$xref), yes = 1 , no = NA)')))
+}
+
+
+orpha <- orpha |> mutate(xref_new = str_extract_all(xref_new, pattern = "[^;]*:"))
+
 
