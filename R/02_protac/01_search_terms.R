2b6c641fe8893addfb6d27bded35f57dc673c80a
Sowmya Parthiban <sparthi1@jh.edu>
2023-01-12 20:12:04 +0530

    more notes on extraction

--- R/02_protac/01_search_terms.R
+++ R/02_protac/01_search_terms.R
@@ -13,6 +13,9 @@
 
 #Use rvest to read html, if it has the term "zero results",
 #that means we didn't find any articles related to that search term.
+#also look out for spell checks and NA values in output file.
+#e.g. when performing a pubmed search with the term "bleep" it can get autocorrected
+# to "sleep", so we need to look out for that.
 
 
 ####LOAD PACKAGES ####
@@ -23,13 +26,11 @@ library(readr)
 library(tidyr) #crossing() function
 library(stringr)
 library(purrr)
-
 library(parallel)
-detectCores() 8
-
 
 #### load data ####
 
+#use warhead as an example
 warhead_db <- read_csv(here("data", "raw_data", "warhead.csv"))
 
 
@@ -51,15 +52,16 @@ ordo <- read_csv(here("data", "processed_data", "ordo_post_02.csv"))
 # [5] "Sclerosing perineurioma"                                "Extraneural perineurioma"
 
 
+## use crossing function from tidyr to create cartesian product
 warhead_Name_crossing <- crossing(warhead_db$Name, ordo$name)
 colnames(warhead_Name_crossing) <- c("term_1", "term_2")
 
-#remove extra spaces from terms
+#### remove extra spaces from terms and create new columns ####
 
 warhead_Name_crossing$term_1 <- gsub("\\s+"," ",warhead_Name_crossing$term_1)
 warhead_Name_crossing$term_2 <- gsub("\\s+"," ",warhead_Name_crossing$term_2)
 
-# URLencode terms
+##### URLencode terms and check time it takes to encode ####
 t1 <- Sys.time()
 warhead_Name_crossing <- warhead_Name_crossing |>
     mutate(term_1_url_encode = URLencode(term_1))
@@ -72,13 +74,15 @@ t2 - t1 #Time difference of 1.74198 mins
 t3 - t2 #Time difference of 3.138833 mins
 
 
+## create new column called url that puts term1 and term2 together along with url prefix
 warhead_Name_crossing <- warhead_Name_crossing |>
     mutate(url = paste0("https://pubmed.ncbi.nlm.nih.gov/?term=%28%28",
            term_1_url_encode, "+AND+", term_2_url_encode))
 
 
-
 #### FUNCTIONS FOR GETTING RELEVANT INFO OUT OF HTML PAGE ####
+
+#gets number of results for the combination
 get_num_results <- function(url){
     html_data <- read_html(url)
     res <- (html_data |> html_nodes("div.results-amount") |> html_text())[1]
@@ -87,12 +91,13 @@ get_num_results <- function(url){
     res
 }
 
-
+#checks if there's a spell check section in the resulting page
 is_spell_check_warning_na <- function(url){
     html_data <- read_html(url)
     res <- (html_data |> html_nodes("corrected-query-warning") |> html_text())[1]
     is.na(res)
 }
+
 
 
 ###### CLEAN AND TABULATE EXTERNAL INFO FOR ALL OBS ####
@@ -102,7 +107,8 @@ t5 <- Sys.time()
 
 
 t6 <- Sys.time()
-warhead_Name_crossing$is_spell_checked <- map(warhead_Name_crossing$url, is_spell_check_warning_na)
+warhead_Name_crossing$is_spell_checked <- map(warhead_Name_crossing$url,
+                                              is_spell_check_warning_na)
 t7 <- Sys.time()
 
 t5-t4
@@ -111,31 +117,11 @@ t5-t4
 t7 - t6
 
 
-###### PARALLEL COMPUTATION #####
-s <- system.time({  warhead_Name_crossing$num_results<- mclapply(warhead_Name_crossing$url,
-                                    get_num_results, mc.cores = 4) })
-# user  system elapsed
-# 0.220   0.115   1.858
-
-### Reduced from 6 to 2 seconds.
-
-warhead_Name_crossing_fraxf$num_results<- mclapply(warhead_Name_crossing_fraxf$url,
-                                                                 get_num_results, mc.cores = 4)
-# user  system elapsed
-# 6.807   1.422  44.574
-
-warhead_Name_crossing_fraxf$is_not_spell_checked <- mclapply(warhead_Name_crossing_fraxf$url,
-                                                         is_spell_check_warning_na, mc.cores = 4)
-
-
-warhead_Name_crossing_fraxf
-
-
-
 
 ######## Steroid-responsive encephalopathy #########
 #ORDO ID: 83601
 #Steroid-responsive encephalopathy
+
 
 warhead_Name_crossing_sreat <- warhead_Name_crossing |>
     filter(grepl("Steroid-responsive encephalopathy", term_2))
